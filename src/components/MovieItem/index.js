a943ccf86d97f36e0e5d6182fb0bd600945c91ca
MRanjith26 <mranjith.2609@gmail.com>
2024-02-11 07:33:59 +0000

    added react-player functionality

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,13 +1,43 @@
 // Write your code here
 import './index.css'
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+
+import {IoMdClose} from 'react-icons/io'
 
 const MovieItem = props => {
   const {movieDetails} = props
-  const {thumbnailUrl} = movieDetails
+  const {thumbnailUrl, videoUrl} = movieDetails
   return (
-    <li className="image-item">
-      <img src={thumbnailUrl} alt="thumbnail" className="movie-image" />
-    </li>
+    <Popup
+      modal
+      trigger={
+        <li className="image-item">
+          <img src={thumbnailUrl} alt="thumbnail" className="movie-image" />
+        </li>
+      }
+    >
+      {close => (
+        <div className="popup-card">
+          <button
+            type="button"
+            className="close-button"
+            onClick={() => close()}
+            data-testid="closeButton"
+            aria-label="close"
+          >
+            <IoMdClose className="close-icon" />
+          </button>
+          <ReactPlayer
+            url={videoUrl}
+            controls="true"
+            height={320}
+            width="90%"
+            padding={20}
+          />
+        </div>
+      )}
+    </Popup>
   )
 }
 export default MovieItem
